8fb9d7b6f294cd4c3d8f4425395e81d8974e642f
João Gabriel Bento <joaogabriel.bmm@gmail.com>
2023-07-30 15:56:17 -0300

    test tty

--- math_test.go
+++ math_test.go
@@ -6,6 +6,6 @@ func TestSum(t *testing.T) {
 	total := Sum(15, 15)
 
 	if total != 30 {
-		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado: %d.", total, 30)
+		t.Errorf("Resultado da soma é inválido: Resultado é %d. Esperado: %d.", total, 30)
 	}
 }
